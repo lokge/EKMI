8bfaf8e3531a6c07564566fe78c2f261c8892e4c
lokge <molnia.erkin@gmail.com>
2022-12-03 21:26:43 +0600

    after all my changes catalog3

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,13 +1,16 @@
 let catalogList = document.querySelector('.catalog__main-content')
 let filterSelectorCatalog = document.querySelector('.catalog__top-select')
+let filterSearch = document.querySelector('.catalog__top-input')
 
 let url = 'http://localhost:6969/items?'
 
-let filterCatalog = ''
+let filterCatalog = '';
+let filterSearchValue = '';
+
 
 const getCatalogItems = () => {
     catalogList.innerHTML = ''
-    fetch(url + `${filterCatalog.length ? 'model=' + filterCatalog : ''}`)
+    fetch(url + `${filterCatalog.length ? 'model=' + filterCatalog + '&' : ''}${filterSearchValue.length ? 'title_like=' + filterSearchValue + '&' : ''}`)
         .then((resolve) => resolve.json())
         .then((resolve) => {
             resolve.forEach((item) => {
@@ -22,9 +25,27 @@ const getCatalogItems = () => {
         .catch((err) => alert(err))
 }
 
+const debounce = (fn, delay) => {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        let fnCall = () => {
+            fn(...args);
+        };
+        timeout = setTimeout(fnCall, delay);
+    };
+};
+
+
 filterSelectorCatalog.addEventListener('change', (e) => {
     filterCatalog = e.target.value
     getCatalogItems()
 })
+
+filterSearch.addEventListener('keyup', (e) => {
+    filterSearchValue = e.target.value;
+    debounce(getCatalogItems(), 2000);
+});
+
 
 getCatalogItems()
